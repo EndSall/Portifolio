7f71d602a94197a85e1b1b640cef056571a97100
EndSall <sallesendrew2@gmail.com>
2025-05-21 16:03:41 -0300

    Criado a conexão ao banco de dados

---
+++ Projeto_Aula13/src/data/connection.js
@@ -0,0 +1,18 @@
+const myslq = require("mysql2")
+
+const connection = myslq.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"consultas_medicas"
+})
+
+connection.connect((err)=>{
+    if(err){
+        console.log("Erro ao conectar ao MySql",err)
+        return;
+    }
+    console.log("Conectado ao MySQL!")
+})
+
+module.exports = connection
