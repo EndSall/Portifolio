a42d4955b28a7af3e0397ea2c33557718038d57c
EndSall <sallesendrew2@gmail.com>
2025-05-27 13:54:48 -0300

    Atualização: Criando um caminho para o frontend, e organizando as pastas de front

--- Projeto_Aula13/Backend/src/app.js
+++ Projeto_Aula13/Backend/src/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const path = require("path")
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname,"../FrontEnd/")))
+
 
 //___Rotas___
-/* 
-const routesFront = require("./routes/routes_Front.js")
+
+const routesFront = require("./routes/FrontEnd/routes_Frontend.js")
 app.use("/",routesFront)
-*/
+
 const routesPacientes = require("./routes/routes_Pacientes.js")
 app.use("/api/pacientes",routesPacientes)
 
